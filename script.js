42785564831cf5239591ec52fcc8465f988c5c39
Jacob Taylor <jacob.taylor0299@gmail.com>
2020-10-17 21:22:07 -0700

    Main Style Elements

--- script.js
+++ script.js
@@ -17,39 +17,51 @@ function updatePage(url) {
   fetch(url)
     .then(function(got) {
       if (got.status != 200) {
-        document.getElementById("multiverseResults").innerHTML = "<p>Your search returned no results</p>";
+        document.getElementById("multiverseResults").innerHTML = "";
+        var errorMessage = document.createElement('h2');
+        errorMessage.innerHTML = "Your search returned no results!";
+        document.getElementById("multiverseResults").appendChild(errorMessage);
         return {text: "Error calling API service: " + got.statusText}
       }
       return got.json();
     }).then(function(json){
       console.log(json);
-      document.getElementById("multiverseResults").innerHTML = "";
-      for (let i = 0; i < json.results.length; i++) {
-        document.getElementById("multiverseResults").innerHTML += makeResult(json.results[i]);
+      if (!json.text) {
+        document.getElementById("multiverseResults").innerHTML = "";
+        for (let i = 0; i < json.results.length; i++) {
+          document.getElementById("multiverseResults").innerHTML += makeResult(json.results[i]);
+        }
+        return json.info;
       }
-      return json.info;
     }).then(function(info) {
-      if (info.prev !== null) {
-        var prevBtn = document.createElement("BUTTON");
-        prevBtn.innerHTML = "Prev";
-        prevBtn.setAttribute("id", "prev");
+      if (info !== undefined){
+        var buttonDiv = document.createElement('div');
+        buttonDiv.setAttribute('id', 'buttonDiv');
+        document.getElementById("multiverseResults").appendChild(buttonDiv);
+        if (info.prev !== null) {
+          var prevBtn = document.createElement("BUTTON");
+          prevBtn.innerHTML = "Prev";
+          prevBtn.setAttribute("id", "prev");
 
-        document.getElementById("multiverseResults").appendChild(prevBtn);
-        document.getElementById('prev').addEventListener('click', function(prevPage){
-          prevPage.preventDefault();
-          updatePage(info.prev);
-        });
-      }
-      if (info.next !== null) {
-        var nextBtn = document.createElement("BUTTON");
-        nextBtn.innerHTML = "Next";
-        nextBtn.setAttribute("id", "next");
+          document.getElementById("buttonDiv").appendChild(prevBtn);
+          document.getElementById('prev').addEventListener('click', function(prevPage){
+            prevPage.preventDefault();
+            window.scrollTo(0,600);
+            updatePage(info.prev);
+          });
+        }
+        if (info.next !== null) {
+          var nextBtn = document.createElement("BUTTON");
+          nextBtn.innerHTML = "Next";
+          nextBtn.setAttribute("id", "next");
 
-        document.getElementById("multiverseResults").appendChild(nextBtn);
-        document.getElementById('next').addEventListener('click', function(nextPage){
-          nextPage.preventDefault();
-          updatePage(info.next);
-        });
+          document.getElementById("buttonDiv").appendChild(nextBtn);
+          document.getElementById('next').addEventListener('click', function(nextPage){
+            nextPage.preventDefault();
+            window.scrollTo(0,600);
+            updatePage(info.next);
+          });
+        }
       }
     });
 }
@@ -57,12 +69,13 @@ function updatePage(url) {
 function makeResult(info) {
   let returnHTML = "<div class=\"result\">\n";
   if (info.image) {
-    returnHTML += "<img src=\"" + info.image + "\"/>\n";
+    returnHTML += "<div class=\"profilePic\"><img src=\"" + info.image + "\"/></div>\n";
   }
+  returnHTML += "<div class=\"infoDiv\">"
   if (info.name) {
     returnHTML += "<h2>" + info.name + "</h2>\n";
   }
-  if (info.episode) {
+  if (info.episode && !info.species) {
     returnHTML += "<h4><em>" + info.episode + "</em></h4>\n"
   }
   if (info.air_date) {
@@ -90,7 +103,7 @@ function makeResult(info) {
   if (info.origin) {
     returnHTML += "<li>Origin: " + info.origin.name +"</li>\n"
   }
-  returnHTML += "</ul>\n"
+  returnHTML += "</ul>\n</div>\n"
   returnHTML += "</div>";
   return returnHTML;
 }
